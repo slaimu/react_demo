bc89ab43708634982f0f8392cb0964fd4b2e3518
Xiaowu Zhang <xiaowu.zhangv@gmail.com>
2016-10-15 17:03:14 +0800

    project config with a hello world example

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var gulpif = require('gulp-if');
+var streamify = require('gulp-streamify');
+var autoprefixer = require('gulp-autoprefixer');
+var cssmin = require('gulp-cssmin');
+var less = require('gulp-less');
+var concat = require('gulp-concat');
+var plumber = require('gulp-plumber');
+var source = require('vinyl-source-stream');
+var babelify = require('babelify');
+var browserify = require('browserify');
+var uglify = require('gulp-uglify');
+
+var production = process.env.NODE_ENV === 'production';
+
+var dependencies = [
+  'react',
+  'react-dom'
+];
+var entryFiles = [
+  './demo1/index.js'
+];
+/*
+ |--------------------------------------------------------------------------
+ | Compile third-party dependencies separately for faster performance.
+ |--------------------------------------------------------------------------
+ */
+gulp.task('browserify-vendor', function() {
+  return browserify()
+    .require(dependencies)
+    .bundle()
+    .pipe(source('vendor.bundle.js'))
+    .pipe(gulpif(production, streamify(uglify({ mangle: false }))))
+    .pipe(gulp.dest('build/'));
+});
+
+/*
+ |--------------------------------------------------------------------------
+ | Compile only project files, excluding all third-party dependencies.
+ |--------------------------------------------------------------------------
+ */
+gulp.task('browserify', ['browserify-vendor'], function() {
+  return browserify({entries: [entryFiles]})
+    .external(dependencies)
+    .transform(babelify)
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulpif(production, streamify(uglify({ mangle: false }))))
+    .pipe(gulp.dest('build/'));
+});
+gulp.task('default', ['browserify']);
+gulp.task('build', ['browserify'])
